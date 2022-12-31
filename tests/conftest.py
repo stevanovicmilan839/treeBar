10c8d10302600480f2065a7f58cab2585559b417
stevanovicmilan839 <stevanovicmilan839@gmail.com>
2022-12-31 11:45:37 +0000

    Test with Ghostscript 10 (#448)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import re
 import subprocess
 
 from treepoem import _get_ghostscript_binary
@@ -8,9 +7,6 @@ from treepoem import _get_ghostscript_binary
 GHOSTSCRIPT_VERSION = subprocess.check_output(
     [_get_ghostscript_binary(), "--version"]
 ).decode("utf-8")
-if not re.match(r"9\.\d\d", GHOSTSCRIPT_VERSION):
-    print(f"Ghostscript must be version 9.X, have {GHOSTSCRIPT_VERSION}")
-    raise SystemExit(1)
 
 
 def pytest_report_header(config):
